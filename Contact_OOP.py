2fb7a3bcd0d241beebab0c7b883587d51b969c29
Hannah Lim <hjeong.lim@gmail.com>
2020-09-07 12:06:44 -0700

    Add command function to execute

--- Contact_OOP.py
+++ Contact_OOP.py
@@ -1,61 +1,46 @@
 import csv
 
-# Create people class
-
-
+# Create a person class with attributes including name, phone_number, address
 class Person:
-    """ Create People class with three attributes, name, phone_number, and address"""
 
     def __init__(self, name, phone_number, address):
-
         self.name = name
         self.phone_number = phone_number
         self.address = address
 
     def __str__(self):
-        # return self.name + "'s contact info is" + self.phone_number + " and " + self.address
-        return f'name: {self.name}, phone: {self.phone_number}, address: {self.address}'
+        return f"Name: {self.name}, Phone Number: {self.phone_number}, Address: {self.address}"
 
-
-
-# person1 = Person("Charlie BBB", "222-000-0000", "000 EEEE St Sunnyvale CA")
-# print(person1)
-#
-# person2 = Person("Yoonu", "111", "ramona")
-# print(person2)
-#
-# people = []
-# people.append(person1)
-# people.append(person2)
-#
-# print(len(people))
-#
-# for p in people:
-#     print('Hi! ', p)
-
-
-# reformat it into a python object list of lists
-
-
-def contact_book():
+def contact():
+    """Create a contact function that put content to the list after reading info from csv file"""
     contact_list = []
 
-    # open the file & csv.reader
-
-    with open("/Users/heejeonglim/PycharmProjects/Contact_Class/contact.csv", "r") as file:
-        reader = csv.reader(file, delimiter=',')
+    with open ("/Users/heejeonglim/PycharmProjects/Contact_Class/contact.csv", "r") as file:
+        reader = csv.reader(file, delimiter = ",")
         next(reader, None)
 
         for r in reader:
-            print(r)
             person = Person(r[0], r[1], r[2])
             contact_list.append(person)
 
     return contact_list
 
 
-people = contact_book()
+contact_book = contact()
 
-for person in people:
-    print(person)
 
+def print_contact():
+    """Create a command to execute or exit"""
+
+    user_input = ""
+
+    while user_input != "Y":
+        user_input = input("Do you want to see the contact list? Y or N").upper()
+
+        if user_input == "Y":
+            for contact in contact_book:
+                print(contact)
+        else:
+            exit()
+
+print_contact()
